4cc5579fef62af8275f8f0f001edac40410c3130
Daniel Frąk <dan.frak@gmail.com>
2020-03-18 18:13:07 +0100

    Initial commit

---
+++ src/main/java/dev/codesoapbox/springbootkibanademo/SpringBootKibanaDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.codesoapbox.springbootkibanademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKibanaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKibanaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/codesoapbox/springbootkibanademo/SpringBootKibanaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.codesoapbox.springbootkibanademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKibanaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
